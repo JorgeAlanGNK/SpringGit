4b205f6cf6018d7d3be8ecd264643ec8ec60f091
JorgeAlanGNK <rygenkio.minstar45@gmail.com>
2026-04-13 22:39:11 -0600

    Jorge 13042026
    Se ha logrado realizar el collapse

--- src/main/java/com/jorge_alan/spring_git_mvc/componentes/Principal.java
+++ src/main/java/com/jorge_alan/spring_git_mvc/componentes/Principal.java
@@ -5,16 +5,24 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import com.jorge_alan.spring_git_mvc.componentes.events.EventosStruct.PrincipalEvento;
+
 import java.util.List;
+
+import com.google.common.base.Strings;
 import com.jorge_alan.spring_git_mvc.modelos.CapaModelo.EstructuraPanel;
 import com.kitfox.svg.app.beans.SVGIcon;
 
@@ -33,6 +41,18 @@ public final class Principal {
         @Getter
         @Setter
         private List<EstructuraPanel> paneles;
+
+        private static final String BUSCAR_CONST = "buscar...";
+        private static final String EMPTY_STRING = Strings.nullToEmpty("");
+
+        private static final PrincipalEvento _eventosPrincipal = IniciarEventos();
+
+        private static PrincipalEvento IniciarEventos() {
+            if (_eventosPrincipal == null) {
+                return new PrincipalEvento();
+            }
+            return _eventosPrincipal;
+        }
 
         @Override
         public void Builder() {
@@ -63,49 +83,162 @@ public final class Principal {
         }
 
         private void layoutBarra(final JPanel componente) {
-            JPanel seccionRama = new JPanel();
+            JPanel ramas = new JPanel();
             JPanel stashes = new JPanel();
             JPanel remotos = new JPanel();
-            this.paneles.add(EstructuraPanel.builder().etiqueta("seccionRama").actualComponente(seccionRama).build());
-            this.paneles.add(EstructuraPanel.builder().etiqueta("stashes").actualComponente(stashes).build());
-            this.paneles.add(EstructuraPanel.builder().etiqueta("remotos").actualComponente(remotos).build());
+            JPanel ramaFlow = new JPanel();
+            JPanel stashFlow = new JPanel();
+            JPanel remotosFlow = new JPanel();
+            JPanel ramaCollapse = new JPanel();
+            JPanel stashCollapse = new JPanel();
+            JPanel remotosCollapse = new JPanel();
+            ramaCollapse.setVisible(false);
+            stashCollapse.setVisible(false);
+            remotosCollapse.setVisible(false);
+            this.paneles.add(new EstructuraPanel("seccionRama", ramas, false));
+            this.paneles.add(new EstructuraPanel("stashes", stashes, false));
+            this.paneles.add(new EstructuraPanel("remotos", remotos, false));
+            //Faltaba el panel a Generar
+            this.paneles.add(new EstructuraPanel("seccionRama", ramaCollapse, false));
+            this.paneles.add(new EstructuraPanel("stashes", stashCollapse, false));
+            this.paneles.add(new EstructuraPanel("stashes", remotosCollapse, false));
+            // va atraer el doble
             GridBagLayout grbly = new GridBagLayout();
             GridBagConstraints grbcs = new GridBagConstraints();
             componente.setLayout(grbly);
-            for (int i = 0; i < this.paneles.size(); i++) {
+            for (int i = 0; i < this.paneles.size() && i < 3; i++) {
                 grbcs.gridx = 0;
                 grbcs.gridy = i;
                 grbcs.weighty = 0;
                 grbcs.weightx = 1;
                 grbcs.fill = GridBagConstraints.BOTH;
                 grbcs.insets = new Insets(5, 20, 5, 20);
-                componente.add(this.paneles.get(i).getActualComponente(), grbcs);
-                AreaComponente(this.paneles.get(i).getActualComponente(), new FlowLayout(FlowLayout.CENTER), new JTextField("buscar..."), this.paneles.get(i).getEtiqueta());
+                componente.add(this.paneles.get(i).getActualCollapse(), grbcs);
             }
+            JTextField buscador1 = new JTextField();
+            buscador1.setPreferredSize(new Dimension(120, 30));
+            buscador1.setText(BUSCAR_CONST);
+            AreaComponente(ramas, ramaFlow, new FlowLayout(FlowLayout.CENTER),
+                    buscador1, "seccionRama");
+            JTextField buscador2 = new JTextField();
+            buscador2.setPreferredSize(new Dimension(120, 30));
+            buscador2.setText(BUSCAR_CONST);
+            AreaComponente(stashes, stashFlow, new FlowLayout(FlowLayout.CENTER),
+                    buscador2, "stashes");
+            JTextField buscador3 = new JTextField();
+            buscador3.setPreferredSize(new Dimension(120, 30));
+            buscador3.setText(BUSCAR_CONST);
+            AreaComponente(remotos, remotosFlow, new FlowLayout(FlowLayout.CENTER),
+                    buscador3, "remotos");
         }
 
         private void layoutContenido(final JPanel componente) {
 
         }
 
-        private void AreaComponente(final JPanel panelArea, LayoutManager managerLayout, JTextField buscador, String areaEtiqueta) {
+        private void AreaComponente(final JPanel panelActual, final JPanel areaFlow, LayoutManager managerLayout,
+                JTextField buscador,
+                String areaEtiqueta) {
             try {
-                JLabel areaIcon = null;
-                panelArea.setLayout(managerLayout);
+                BoxLayout layoutTemp = new BoxLayout(panelActual, BoxLayout.Y_AXIS);
+                panelActual.setLayout(layoutTemp);
+                areaFlow.setLayout(managerLayout);
                 SVGIcon icon_collapse_button = new SVGIcon();
-                icon_collapse_button.setSvgURI(BuildLayoutGlass.class
+                Class<BuildLayoutGlass> recursoArea = BuildLayoutGlass.class;
+                icon_collapse_button.setSvgURI(recursoArea
                         .getClassLoader()
                         .getResource("static/arrow-down-svgrepo-com.svg")
                         .toURI());
                 icon_collapse_button.setPreferredSize(new Dimension(20, 20));
                 icon_collapse_button.setScaleToFit(true);
                 JButton btn_collapse_area = new JButton();
+                if (areaEtiqueta == "seccionRama") {
+                    btn_collapse_area.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            _eventosPrincipal.CollapseBranch(e, panelActual, paneles.get(3));
+                        }
+                    });
+                } else if (areaEtiqueta == "stashes") {
+                    btn_collapse_area.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            _eventosPrincipal.CollapseStash(e, panelActual, paneles.get(4));
+                        }
+                    });
+                } else if (areaEtiqueta == "remotos") {
+                    btn_collapse_area.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            _eventosPrincipal.CollapseRemote(e, panelActual, paneles.get(5));
+                        }
+                    });
+                }
                 btn_collapse_area.setIcon(icon_collapse_button);
                 btn_collapse_area.setPreferredSize(new Dimension(30, 40));
-                //personalizacion del buscador
-                buscador.setPreferredSize(new Dimension(70, 20));
-                panelArea.add(btn_collapse_area);
-                panelArea.add(buscador);
+                areaFlow.add(btn_collapse_area);
+                areaFlow.add(buscador);
+                // aqui va el switch, para representar el área correspondiente
+                panelActual.add(areaFlow);
+                switch (areaEtiqueta) {
+                    case "seccionRama" -> {
+                        JLabel zona_etiqueta = new JLabel();
+                        JLabel icon_representate = new JLabel();
+                        // branch-svgrepo-com
+                        SVGIcon icon_zona = new SVGIcon();
+                        icon_zona.setPreferredSize(new Dimension(30, 30));
+                        icon_zona.setSvgURI(recursoArea
+                                .getClassLoader()
+                                .getResource("static/branch-svgrepo-com.svg")
+                                .toURI());
+                        icon_zona.setScaleToFit(true);
+                        zona_etiqueta.setPreferredSize(new Dimension(80, 50));
+                        icon_representate.setPreferredSize(new Dimension(120, 50));
+                        icon_representate.setIcon(icon_zona);
+                        zona_etiqueta.setText("BRANCHES");
+                        areaFlow.add(zona_etiqueta);
+                        areaFlow.add(icon_representate);
+                        panelActual.add(paneles.get(3).getActualCollapse());
+                    }
+                    case "stashes" -> {
+                        JLabel zona_etiqueta = new JLabel();
+                        JLabel icon_representate = new JLabel();
+                        // git-compare-svgrepo-com
+                        SVGIcon icon_zona = new SVGIcon();
+                        icon_zona.setPreferredSize(new Dimension(30, 30));
+                        icon_zona.setSvgURI(recursoArea
+                                .getClassLoader()
+                                .getResource("static/git-compare-svgrepo-com.svg")
+                                .toURI());
+                        icon_zona.setScaleToFit(true);
+                        icon_representate.setPreferredSize(new Dimension(120, 50));
+                        zona_etiqueta.setPreferredSize(new Dimension(80, 50));
+                        icon_representate.setIcon(icon_zona);
+                        zona_etiqueta.setText("STASHES");
+                        areaFlow.add(zona_etiqueta);
+                        areaFlow.add(icon_representate);
+                        panelActual.add(paneles.get(4).getActualCollapse());
+                    }
+                    case "remotos" -> {
+                        JLabel zona_etiqueta = new JLabel();
+                        JLabel icon_representate = new JLabel();
+                        // git-svgrepo-com
+                        SVGIcon icon_zona = new SVGIcon();
+                        icon_zona.setPreferredSize(new Dimension(30, 30));
+                        icon_zona.setSvgURI(recursoArea
+                                .getClassLoader()
+                                .getResource("static/git-svgrepo-com.svg")
+                                .toURI());
+                        icon_zona.setScaleToFit(true);
+                        zona_etiqueta.setPreferredSize(new Dimension(80, 50));
+                        icon_representate.setPreferredSize(new Dimension(120, 50));
+                        icon_representate.setIcon(icon_zona);
+                        zona_etiqueta.setText("REMOTES");
+                        areaFlow.add(zona_etiqueta);
+                        areaFlow.add(icon_representate);
+                        panelActual.add(paneles.get(5).getActualCollapse());
+                    }
+                }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
                 System.out.println(e.getLocalizedMessage());
--- src/main/java/com/jorge_alan/spring_git_mvc/modelos/CapaModelo.java
+++ src/main/java/com/jorge_alan/spring_git_mvc/modelos/CapaModelo.java
@@ -5,19 +5,34 @@ import javax.swing.JPanel;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CapaModelo {
 
+    //las estructuras pertenecen a paneles y operaciones de componentes
     @Builder
     @AllArgsConstructor(access = AccessLevel.PUBLIC)
     public static final class EstructuraPanel {
-        @Getter
-        private String etiqueta;
-        @Getter
-        private JPanel actualComponente;
+        @Getter private String etiqueta;
+        @Getter private JPanel actualCollapse;
+        @Getter @Setter private boolean esVisible;
+    }
+
+    @NoArgsConstructor
+    public static final class RamaModelo {
+        @Getter @Setter private String nombreRama;
+        @Getter @Setter private List<String> Carpeta;
+        @Getter @Setter private boolean carpeta;
+        @Getter @Setter private boolean origin;
+    }
+
+    @NoArgsConstructor
+    public static class StashModelo {
+       @Getter @Setter private List<String> stashes;
     }
 }
