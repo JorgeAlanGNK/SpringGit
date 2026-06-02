f2f53deba23812413754e53da836f70de26f29cb
Jorge Alan <114108535+JorgeAlanGNK@users.noreply.github.com>
2026-06-02 10:24:18 -0600

    avance nuevo en DEV (#15)
    * añadi nuevas imagenes
    
    * nueva imagen reemplazada
    
    * nueva imagen reemplazada, no se subio
    
    * cambios obligatorios para swing
    
    * campos de evaluacion terminados, falta la de busqueda

---
+++ src/main/java/com/jorge_alan/spring_git_mvc/componentes/extension/IconoExtension.java
@@ -0,0 +1,9 @@
+package com.jorge_alan.spring_git_mvc.componentes.extension;
+
+import javax.swing.Icon;
+
+public interface IconoExtension {
+
+    Icon MostrarIcono(String nombre, int w, int h);
+    Icon MostrarIconoDesktop(String nombre, int w, int h);
+}
